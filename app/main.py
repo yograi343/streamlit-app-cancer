0065b8677554dfaf41128377955f4297c997707b
Yogesh Rai <yograi343@gmail.com>
2024-03-28 15:11:57 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -184,7 +184,7 @@ def main():
         layout="wide",
         initial_sidebar_state="expanded",
     )
-    with open("..\\assets\\style.css") as f:
+    with open("/assets/style.css") as f:
         st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 
     with st.container():
